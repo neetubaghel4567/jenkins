869dc4fb47146d1dd1e1929b75040967178c2d59
nehapatel345 <99860862+nehapatel345@users.noreply.github.com>
2023-02-17 16:55:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
+resource "google_compute_instance" "default" {
+  name         = "neha-vm"
+  machine_type = "e2-small"
+  project = "inavolan"
+  zone         = "us-central1-c"
+
+  tags = ["allow-firewall"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+  }
+  allow_stopping_for_update = true
+
+}
 
