28d66dcafc1b696f54a48c0072966849c35e3e53
nehapatel345 <99860862+nehapatel345@users.noreply.github.com>
2023-02-24 13:46:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
-
+ project ="magnetic-quasar-378605"
   
 
   boot_disk {
