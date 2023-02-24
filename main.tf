e10322df3b07be968ba50a827ce6495afdc230b4
nehapatel345 <99860862+nehapatel345@users.noreply.github.com>
2023-02-24 18:36:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-resource "google_service_account" "default" {
-  account_id   = "service-accountid"
-  display_name = "Service Account"
-}
+
 
 resource "google_compute_instance" "default" {
   name         = "testvm"
@@ -25,14 +22,5 @@ resource "google_compute_instance" "default" {
     network = "default"
 
 
-  }
-
-
-
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
   }
 }
