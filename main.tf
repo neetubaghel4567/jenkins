5ddbcff6ba23e6a02c896a958461f81e65513184
nehapatel345 <99860862+nehapatel345@users.noreply.github.com>
2023-02-24 11:42:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
   name         = "neha-vm1"
   machine_type = "e2-small"
-  project = "inavolan"
+  project = "Jenkins-server-project"
   zone         = "us-central1-a"
   tags = ["allow-firewall"]
   boot_disk {
